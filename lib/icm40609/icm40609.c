fba93d8fc2567aba4e366fd1b54caded8d2f0b78
Gabe H <gabe.haarberg@gmail.com>
2026-05-24 19:16:56 -0700

    IMU comments

--- lib/icm40609/icm40609.c
+++ lib/icm40609/icm40609.c
@@ -5,6 +5,18 @@
 
 LOG_MODULE_REGISTER(icm, LOG_LEVEL_INF);
 
+/**
+ * @brief Initialize the IMU.
+ * 1) Checks that databus is ready
+ * 2) Confirms WHO_AM_I value
+ * 3) Resets IMU (waits 500ms)
+ * 4) Configures power settings
+ * 5) Configures gyro settings
+ * 6) Configures accel settings
+ *
+ * @param icm
+ * @return int
+ */
 int icm_init(icm_t *icm)
 {
     int ret;
@@ -42,6 +54,13 @@ int icm_init(icm_t *icm)
         return 3;
     }
 
+    ret = icm_select_bank(icm, 0);
+    if (ret)
+    {
+        LOG_WRN("Failed to select bank before resetting board");
+        return ret;
+    }
+
     ret = icm_reset(icm);
     if (ret)
     {
@@ -73,6 +92,13 @@ int icm_init(icm_t *icm)
     return 0;
 }
 
+/**
+ * @brief Resets the IMU. Sleeps for 500ms before exiting to allow for wake-up
+ * NOTE: 500ms was found via trial and error.
+ *
+ * @param icm
+ * @return int
+ */
 int icm_reset(icm_t *icm)
 {
     int ret;
@@ -91,6 +117,18 @@ int icm_reset(icm_t *icm)
     return ret;
 }
 
+/**
+ * @brief Configures power settings on IMU
+ * (At time of writing): 0b00001111);
+ * 7:6: Reserved - (00)
+ *   5: TEMP_DIS - (0) Temperature sensor enabled
+ *   4: IDLE - (0) Entire chip goes to sleep if gyro/accel turned off
+ * 3:2: GYRO_MODE - (11) Gyro in Low-Noise mode
+ * 1:0: ACCEL_MODE - (11) Accel in Low-Noise mode
+ *
+ * @param icm
+ * @return int
+ */
 int icm_init_power(icm_t *icm)
 {
     int ret;
@@ -110,6 +148,16 @@ int icm_init_power(icm_t *icm)
     return ret;
 }
 
+/**
+ * @brief Configures the gyro on the IMU
+ * (At time of writing): 0b00100110
+ * 7:5: GYRO_FS_SEL - (001) +/-1000dps
+ *   4: Reserved - (0)
+ * 3:0: GYRO_ODR - (0110) (default) 1kHz
+ *
+ * @param icm
+ * @return int
+ */
 int icm_init_gyro(icm_t *icm)
 {
     int ret;
@@ -118,7 +166,6 @@ int icm_init_gyro(icm_t *icm)
     if (ret)
         return 1;
 
-    // Set gyro scale range (+/- 1000dps, 1kHz)
     ret = icm_reg_write(icm, ICM_REG_GYRO_CONFIG0, 0b00100110);
     if (ret)
         return 2;
@@ -128,6 +175,16 @@ int icm_init_gyro(icm_t *icm)
     return ret;
 }
 
+/**
+ * @brief Configures the accelerometer on the IMU
+ * (At time of writing): 0b01000110
+ * 7:5: ACCEL_FS_SEL - (010) +/-8g
+ *   4: Reserved
+ * 3:0: ACCEL_ODR - (0110) (default) 1kHz
+ *
+ * @param icm
+ * @return int
+ */
 int icm_init_accel(icm_t *icm)
 {
     int ret;
@@ -146,16 +203,43 @@ int icm_init_accel(icm_t *icm)
     return ret;
 }
 
+/**
+ * @brief Read value from a register on IMU. Result is always 1 byte and placed in `&value`.
+ * Register bank *must* already be selected
+ *
+ * @param icm
+ * @param reg Register to be accessed (1 byte)
+ * @param value Memory location to store result in (1 byte)
+ * @return int
+ */
 int icm_reg_read(icm_t *icm, uint8_t reg, uint8_t *value)
 {
     return i2c_write_read_dt(icm, &reg, sizeof(reg), value, 1);
 }
 
+/**
+ * @brief Write to a register on the IMU. Register bank *must* already be selected.
+ *
+ * @param icm
+ * @param reg Register to write to (1 byte)
+ * @param value Value to write to register (1 byte)
+ * @return int
+ */
 int icm_reg_write(icm_t *icm, uint8_t reg, uint8_t value)
 {
     return i2c_reg_write_byte_dt(icm, reg, value);
 }
 
+/**
+ * @brief Read gyro data scaled properly. Each axis is returned into the `x` `y` and `z`
+ * float pointers in units of [dps].
+ *
+ * @param icm
+ * @param x Angular velocity in X-axis [dps]
+ * @param y Angular velocity in Y-axis [dps]
+ * @param z Angular velocity in Z-axis [dps]
+ * @return int
+ */
 int icm_read_gyro(icm_t *icm, float *x, float *y, float *z)
 {
     raw_gyro_t g;
@@ -172,12 +256,19 @@ int icm_read_gyro(icm_t *icm, float *x, float *y, float *z)
     return ret;
 }
 
+/**
+ * @brief Reads all gyro data (6 bytes total, 2 bytes per axis). Result goes into `&g`.
+ *
+ * @param icm
+ * @param g raw_gyro_t memory location to store result into.
+ * @return int
+ */
 int icm_read_gyro_raw(icm_t *icm, raw_gyro_t *g)
 {
     uint8_t buffer[6];
     uint8_t reg = ICM_REG_GYRO_DATA_X1;
 
-    int ret = i2c_write_read_dt(icm, &reg, sizeof(reg), &buffer, ICM_GYRO_DATA_SIZE);
+    int ret = i2c_write_read_dt(icm, &reg, sizeof(reg), buffer, ICM_GYRO_DATA_SIZE);
 
     if (ret)
     {
@@ -192,6 +283,16 @@ int icm_read_gyro_raw(icm_t *icm, raw_gyro_t *g)
     return ret;
 }
 
+/**
+ * @brief Read gyro data scaled properly. Each axis is returned into the `x` `y` and `z`
+ * float pointers in units of [g]'s.
+ *
+ * @param icm
+ * @param x Acceleration in X-axis [g]
+ * @param y Acceleration in Y-axis [g]
+ * @param z Acceleration in Z-axis [g]
+ * @return int
+ */
 int icm_read_accel(icm_t *icm, float *x, float *y, float *z)
 {
     raw_accel_t a;
@@ -208,13 +309,18 @@ int icm_read_accel(icm_t *icm, float *x, float *y, float *z)
     return ret;
 }
 
+/**
+ * @brief Reads all accel data (6 bytes total, 2 bytes per axis). Result goes into `&a`.
+ *
+ * @param icm
+ * @param g raw_accel_t memory location to store result into.
+ * @return int
+ */
 int icm_read_accel_raw(icm_t *icm, raw_accel_t *a)
 {
     uint8_t buffer[6];
-    uint8_t reg = ICM_REG_ACCEL_DATA_X1;
 
-    // int ret = i2c_write_read_dt(icm, &reg, sizeof(reg), &buffer, ICM_GYRO_DATA_SIZE);
-    int ret = i2c_burst_read_dt(icm, ICM_REG_ACCEL_DATA_X1, &buffer, ICM_ACCEL_DATA_SIZE);
+    int ret = i2c_burst_read_dt(icm, ICM_REG_ACCEL_DATA_X1, buffer, ICM_ACCEL_DATA_SIZE);
 
     if (ret)
     {
@@ -229,7 +335,19 @@ int icm_read_accel_raw(icm_t *icm, raw_accel_t *a)
     return ret;
 }
 
+/**
+ * @brief Select which register bank to access during next read/write command(s).
+ *
+ * @param icm
+ * @param bank Register bank (0-4)
+ * @return int
+ */
 int icm_select_bank(icm_t *icm, uint8_t bank)
 {
+    if (bank > 4)
+    {
+        return 1;
+    }
+
     return icm_reg_write(icm, ICM_REG_BANK_SEL, (bank & 0b111));
 }
