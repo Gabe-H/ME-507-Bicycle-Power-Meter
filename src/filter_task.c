0fdfd746062ad69b582e58d25e61c189d37a3ba2
Gabe H <gabe.haarberg@gmail.com>
2026-05-31 19:29:37 -0700

    Fix angle calculation

--- src/filter_task.c
+++ src/filter_task.c
@@ -9,31 +9,56 @@ void filter_task(void)
     while (1)
     {
         struct imu_data_t *data = k_fifo_get(&imu_fifo, K_FOREVER);
+        float ax = data->accel_x;
+        float ay = data->accel_y;
 
         k_mem_slab_free(&imu_data_slab, (void *)data);
 
         float angle = 0;
+        int state = 0;
 
-        if (data->accel_x == 0)
+        // Check if atan denominator is 0 first
+        if (ax == 0.0F)
         {
-            angle = 1.5708;
+            if (ay < 0.0F)
+                angle = 4.7124;
+            else
+                angle = 1.5708;
         }
         else
         {
-            angle = atanf(data->accel_y / data->accel_x);
+            angle = atanf(ay / ax);
+
+            // Arctan truth table.
+            // Theta=0 when IMU x axis is facing upwards
+            if (ax > 0.0F && ay > 0.0F)
+            {
+                state = 1;
+                angle = 3.1415F - angle;
+            }
+
+            else if (ax > 0.0F && ay <= 0.0F)
+            {
+                state = 2;
+                angle = 3.1415F - angle;
+            }
+            else if (ax < 0.0F && ay >= 0.0F)
+            {
+                state = 3;
+                angle = -angle;
+            }
+            else if (ax < 0.0F && ay < 0.0F)
+            {
+                angle = 6.2831F - angle;
+            }
 
             // angle += 1.5708; // Offset due to IMU placement
-
-            // Depending on signage of x component, add 180deg offset
-            // to ensure output angle goes from 0-360 (rather than 0-180)
-            // if (data->accel_x < 0)
-            //     angle += 3.1415;
         }
 
         char *mem_ptr = k_malloc(50);
 
         // sprintf(mem_ptr, "Angle: %f", angle);
-        sprintf(mem_ptr, "X: %.2f, Y: %.2f", (double)data->accel_x, (double)data->accel_y);
+        sprintf(mem_ptr, "X: %.2f, Y: %.2f, theta: %.2f, [%d]", (double)data->accel_x, (double)data->accel_y, (double)angle, state);
 
         k_fifo_put(&printk_fifo, mem_ptr);
     }
